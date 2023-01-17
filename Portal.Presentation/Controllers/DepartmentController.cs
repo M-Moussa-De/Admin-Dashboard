562d8ad4ff2b6ff5249f1ba7ca531a84d16e007f
Mahmoud Moussa <m.moussa@ratio-software.de>
2023-01-17 15:04:49 +0100

    Fetch data from db

--- Portal.Presentation/Controllers/DepartmentController.cs
+++ Portal.Presentation/Controllers/DepartmentController.cs
@@ -1,55 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Portal.Business.Repository;
 
 namespace Portal.Presentation.Controllers
 {
     public class DepartmentController : Controller
     {
-        public IActionResult Index()
+        DepartmentRepo repo = new DepartmentRepo();
+        
+        public async Task<IActionResult> Index()
         {
-            Employee emp1 = new Employee()
-            {
-                Id = 1,
-                Name = "Mark",
-                Salary = 1500
-            };
+            var depts = await repo.GetAsync();
 
-            Employee emp2 = new Employee()
-            {
-                Id = 2,
-                Name = "Rebecca",
-                Salary = 1350
-            };
-
-            Employee emp3 = new Employee()
-            {
-                Id = 3,
-                Name = "Alejandra",
-                Salary = 1750
-            };
-
-            Employee emp4 = new Employee()
-            {
-                Id = 4,
-                Name = "Markous",
-                Salary = 1890
-            };
-
-            Employee emp5 = new Employee()
-            {
-                Id = 5,
-                Name = "Alessandro",
-                Salary = 3200
-            };
-
-            List<Employee> emps = new List<Employee>();
-
-            emps.Add(emp1);
-            emps.Add(emp2);
-            emps.Add(emp3);
-            emps.Add(emp4);
-            emps.Add(emp5);
-
-            return View(emps);
+            return View(depts);
         }
     }
 }
