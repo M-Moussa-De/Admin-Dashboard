6cc2c77ab6bcc227dba0855bf5280d072c8d73a3
Mahmoud Moussa <m.moussa@ratio-software.de>
2023-01-17 14:55:00 +0100

    Init infrastructure

---
+++ Portal.Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portal.Infrastructure.Entities;
+
+namespace Portal.Infrastructure.Data
+{
+    public partial class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.\\SQLEXPRESS;database=portalDb;Trusted_Connection=True;Encrypt=False;MultipleActiveResultSets=true");
+        }
+    }
+
+    public partial class ApplicationContext
+    {
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ Portal.Infrastructure/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace Portal.Infrastructure.Entities
+{
+    [Table("Departments")]
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Code { get; set; }
+    }
+}
