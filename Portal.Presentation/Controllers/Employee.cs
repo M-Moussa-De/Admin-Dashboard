b44400d9aa6f40584024649c4e560740e38d34b1
Mahmoud Moussa <m.moussa@ratio-software.de>
2023-01-17 11:59:16 +0100

    UI

---
+++ Portal.Presentation/Controllers/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace Portal.Presentation.Controllers
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public float Salary { get; set; }
+    }
+}
